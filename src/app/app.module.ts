d9cbd54085d5e92f3d4481c9a9a5ade0af9bc338
Mateo Tibaquira <nestormateo@gmail.com>
2021-04-06 19:39:45 -0500

    build: ng add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DynFormsMaterialModule } from '@myndpm/dyn-forms/material';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
     DynFormsMaterialModule.forFeature(),
   ],
